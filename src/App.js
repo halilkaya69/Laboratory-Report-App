f61f0663cdc177c571cecb410a3a9fdfe4d17f39
halilkaya <120128959+halilkaya69@users.noreply.github.com>
2024-05-23 14:35:18 +0300

    mantine kuruldu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import '@mantine/core/styles.css';
+import { ColorSchemeScript } from '@mantine/core';
+import { Center, Box } from '@mantine/core';
 
+function Demo1() {
+  return (
+    <Center maw={400} h={100} bg="var(--mantine-color-gray-light)">
+      <Box bg="var(--mantine-color-blue-light)">All elements inside Center are centered</Box>
+    </Center>
+  );
+}
+ function Demo() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+        <title>My awesome app</title>
+
+        <ColorSchemeScript />
+      </head>
+      <body>{/* Your app here */}</body>
+    </html>
+  );
+}
 function App() {
   return (
     <div className="App">
@@ -17,6 +44,8 @@ function App() {
         >
           Learn React
         </a>
+        <Demo/>
+        <Demo1/>
       </header>
     </div>
   );
